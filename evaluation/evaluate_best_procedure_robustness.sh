15de45504d9688e1911bea9add1ad5a405e514d9
nanbhas <nanbhas@stanford.edu>
2023-01-18 19:26:31 -0800

    sanity check run

---
+++ evaluation/evaluate_best_procedure_robustness.sh
@@ -0,0 +1,79 @@
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/bertbase_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/bertlarge_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/robertabase_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/robertalarge_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/electrasmall_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/electrabase_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/electralarge_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/debertav3base_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/debertav3large_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/pubmedbert_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+python evaluate_models_for_robustness.py\
+    --predictions_fpath '/dataNAS/people/jmz/jmz_code/radiology_nlp/rales/inference/predictions/biolinkbertbase_procedure_test/predictions.json'\
+    --dataset_name mimiciii_ct_procedure \
+    --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/biolinkbertlarge_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/bioclinicalbert_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/gatortron_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/radbert1_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
+
+# python evaluate_models.py\
+#     --predictions_fpath '../inference/predictions/radbert2_procedure_test/predictions.json'\
+#     --dataset_name mimiciii_ct_procedure \
+#     --data_split test
