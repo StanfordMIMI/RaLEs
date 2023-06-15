ad01f6cf853d0059333e3f1528c4ac387ab161fd
Juan Manuel Zambrano Chaves <jmz@stanford.edu>
2023-06-15 00:55:18 -0700

    add vilmedic

--- vilmedic/vilmedic/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-from appdirs import user_cache_dir
-
-DATA_DIR = __file__.replace('vilmedic/constants.py', 'data')
-
-CACHE_DIR = user_cache_dir("vilmedic")
-EXTRA_CACHE_DIR = os.path.join(CACHE_DIR, "extras")
-MODEL_ZOO_CACHE_DIR = os.path.join(CACHE_DIR, "zoo", "models")
--- vilmedic/vilmedic/executors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .trainor import Trainor
-from .validator import Validator
-from .utils import create_data_loader
-from .utils import create_model
