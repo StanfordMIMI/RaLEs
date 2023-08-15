d4e5110a3ec6daf4ee00271bcd02a2e0051c6866
Juan Manuel Zambrano Chaves <jmz@stanford.edu>
2023-08-14 23:33:51 -0700

    added radiology gpt eval

--- inference/radiologygpt/README.md
+++ inference/radiologygpt/README.md
@@ -2,3 +2,14 @@
 This folder implements [RadiologyGPT](https://arxiv.org/pdf/2306.08666.pdf) inference. 
 
 Note that RadiologyGPT was developed to support radiology report summarization. Further, it was trained using the MIMIC-CXR dataset.
+
+
+## Instructions
+
+Run the following command to run inference on MEDIQA 2021:
+
+```bash
+    python eval_radiologygpt.py \
+        --data_file /PATH_TO/mediqa2021_test.jsonl \
+        --output_dir /PATH_TO/OUTPUT_DIR
+```
--- inference/radiologygpt/eval_radiologygpt.py
+++ inference/radiologygpt/eval_radiologygpt.py
@@ -4,87 +4,104 @@ Script generated based on https://huggingface.co/spaces/allen-eric/radiology-gpt
 
 import torch
 from peft import PeftModel
-import transformers
-
-assert (
-    "LlamaTokenizer" in transformers._import_structure["models.llama"]
-), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
 from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig
+import argparse
+import os
+import datasets
+from datasets import load_dataset
+from tqdm import tqdm
+import json
 
-tokenizer = LlamaTokenizer.from_pretrained("chavinlo/alpaca-native")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_file", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--output_file_name", type=str, default="MEDIQA2021_test.json")
+    
+    args = parser.parse_args()
 
-BASE_MODEL = "chavinlo/alpaca-native"
-LORA_WEIGHTS = "allen-eric/lora-alpaca-7b-radiology-full"
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        print(f"Created output directory {args.output_dir}")
+    args.output_file_name = args.output_file_name.replace("_out.json", f"{args.data_file.split('/')[-1].replace('.json', '')}_out.json")
+    args.output_file_name = os.path.join(args.output_dir, args.output_file_name)
 
-if torch.cuda.is_available():
-    device = "cuda"
-else:
-    device = "cpu"
+    return args
 
-try:
-    if torch.backends.mps.is_available():
-        device = "mps"
-except:
-    pass
+def get_model():
+    tokenizer = LlamaTokenizer.from_pretrained("chavinlo/alpaca-native")
+    BASE_MODEL = "chavinlo/alpaca-native"
+    LORA_WEIGHTS = "allen-eric/lora-alpaca-7b-radiology-full"
+    
+    if torch.cuda.is_available():
+            device = "cuda"
+    elif torch.backends.mps.is_available():
+            device = "mps"
+    else:
+            device = "cpu"    
 
-if device == "cuda":
-    model = LlamaForCausalLM.from_pretrained(
-        BASE_MODEL,
-        load_in_8bit=False,
-        torch_dtype=torch.float16,
-        device_map="auto",
-    )
-    model = PeftModel.from_pretrained(
-        model, LORA_WEIGHTS, torch_dtype=torch.float16, force_download=True
-    )
-elif device == "mps":
-    model = LlamaForCausalLM.from_pretrained(
-        BASE_MODEL,
-        device_map={"": device},
-        torch_dtype=torch.float16,
-    )
-    model = PeftModel.from_pretrained(
-        model,
-        LORA_WEIGHTS,
-        device_map={"": device},
-        torch_dtype=torch.float16,
-    )
-else:
-    model = LlamaForCausalLM.from_pretrained(
-        BASE_MODEL, device_map={"": device}, low_cpu_mem_usage=True
-    )
-    model = PeftModel.from_pretrained(
-        model,
-        LORA_WEIGHTS,
-        device_map={"": device},
-    )
+    if device == "cuda":
+        model = LlamaForCausalLM.from_pretrained(
+            BASE_MODEL,
+            load_in_8bit=False,
+            torch_dtype=torch.float16,
+            device_map="auto",
+        )
+        model = PeftModel.from_pretrained(
+            model, LORA_WEIGHTS, torch_dtype=torch.float16, force_download=True
+        )
+    elif device == "mps":
+        model = LlamaForCausalLM.from_pretrained(
+            BASE_MODEL,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            LORA_WEIGHTS,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = LlamaForCausalLM.from_pretrained(
+            BASE_MODEL, device_map={"": device}, low_cpu_mem_usage=True
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            LORA_WEIGHTS,
+            device_map={"": device},
+        )
 
+    if device != "cpu":
+        model.half()
+    model.eval()
+    if torch.__version__ >= "2":
+        model = torch.compile(model)
+
+    return model, tokenizer, device
 
 def generate_prompt(instruction, input=None):
     if input:
         return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
-### Instruction:
-{instruction}
+                ### Instruction:
+                {instruction}
 
-### Input:
-{input}
+                ### Input:
+                {input}
 
-### Response:"""
+                ### Response:"""
     else:
         return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
-### Instruction:
-{instruction}
+                ### Instruction:
+                {instruction}
 
-### Response:"""
+                ### Response:"""
 
-if device != "cpu":
-    model.half()
-model.eval()
 
-if torch.__version__ >= "2":
-    model = torch.compile(model)
+
+
 
 
 def evaluate(
@@ -117,23 +134,33 @@ def evaluate(
         )
     s = generation_output.sequences[0]
     output = tokenizer.decode(s)
-    return output.split("### Response:")[1].strip()
+    return output.split("### Response:")[1].split("</s>")[0].strip()
 
+if __name__=="__main__":
+    args = parse_args()
 
-# g = gr.Interface(
-#     fn=evaluate,
-#     inputs=[
-#         gr.components.Textbox(
-#             lines=2, label="Instruction", placeholder="Tell me about alpacas."
-#         ),
-#         gr.components.Textbox(lines=2, label="Input", placeholder="none"),
-#         gr.components.Slider(minimum=0, maximum=1, value=0.1, label="Temperature"),
-#         gr.components.Slider(minimum=0, maximum=1, value=0.75, label="Top p"),
-#         gr.components.Slider(minimum=0, maximum=100, step=1, value=40, label="Top k"),
-#         gr.components.Slider(minimum=1, maximum=4, step=1, value=4, label="Beams"),
-#         gr.components.Slider(
-#             minimum=1, maximum=512, step=1, value=128, label="Max tokens"
-#         ),
-#     ],
+    dataset = load_dataset("json", data_files={"data": args.data_file})
 
-
+    model, tokenizer, device = get_model()
+    
+    model_predictions = []
+    with tqdm(total=len(dataset['data']), unit="Findings->Impression") as pbar:
+        for i, datapoint in enumerate(dataset['data']):
+            if 'findings' in datapoint:
+                findings = datapoint['findings']
+            else:
+                raise NotImplementedError("Datasets without findings not supported at this time.")
+            if 'study_id' in datapoint:
+                study_id = datapoint['study_id']
+            else:
+                raise NotImplementedError("Datasets without study_id not supported at this time.")
+            
+            predicted_impression = evaluate(f"Derive the impression from findings in the radiology report", f"Findings: {findings}")
+            model_predictions.append({'study_id': study_id, 'predicted_impression': predicted_impression})
+            pbar.update(1)
+            
+    with open(args.output_file_name, 'w') as f:
+        f.write(json.dumps(model_predictions))
+        
+    #Test input, from RadiologyGPT arxiv preprint figure 5
+    # print(evaluate("Derive the impression from findings in the radiology report", "Findings: PA and lateral views of the chest. The previously seen pericardial and pleural effusions have resolved. There is no pneumothorax. There is no consolidation. The cardiac, mediastinal, and hilar contours are normal."))
